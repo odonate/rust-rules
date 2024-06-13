32d7b92b25d4c57eed5ec8a4903927401272e3f5
jackmarsh <jackjack97@hotmail.co.uk>
2024-06-13 22:12:14 +0100

    rm debug

--- scripts/rustc.sh
+++ scripts/rustc.sh
@@ -2,5 +2,4 @@
 export LD_LIBRARY_PATH=~/rust-rules/plz-out/gen/third_party/rust/rust-1.78.0-x86_64-unknown-linux-gnu/rust-std-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib:$LD_LIBRARY_PATH
 export RUST_LIB_PATH=~/rust-rules/plz-out/gen/third_party/rust/rust-1.78.0-x86_64-unknown-linux-gnu/rust-std-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib
 export RUSTFLAGS="-L $RUST_LIB_PATH"
-echo "$@"
 exec plz run //third_party/rust:toolchain_rustc -- "$@"
