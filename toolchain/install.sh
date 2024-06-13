9fa3ce82260041cc3f5037379738c3086ecd8932
jackmarsh <jackjack97@hotmail.co.uk>
2024-06-13 09:49:01 +0100

    add script to construct absolute -L path to toolchain

--- toolchain/install.sh
+++ toolchain/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 plz build //third_party/rust:toolchain_deb
-plz query output //third_party/rust/... | grep .deb | while read -r line ; do
+plz query output //third_party/rust:toolchain_deb | grep .deb | while read -r line ; do
     sudo dpkg -i $line
 done
 sudo chmod +x /opt/rust-rules/toolchain/1.0/bin/*
